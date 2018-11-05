b9561fb384b20a3744f64729c4144c6eec73a0b9
James Lee <jamesl33info@gmail.com>
2018-11-05 23:46:57 +0000

    Improve error/warning messages in forms

--- src/components/forms/field.jsx
+++ src/components/forms/field.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const renderField = ({ input, type, placeholder, meta: { touched, error, warning } }) => (
     <div>
-        <input {...input} type={type} placeholder={placeholder} />
-        {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        <input {...input} type={type} placeholder={(touched && ((error) || (warning))) || placeholder}/>
     </div>
 )
 
