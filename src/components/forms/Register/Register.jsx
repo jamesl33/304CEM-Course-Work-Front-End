6ceab3cb78019eeb2cb3834c0056dcc4e0ab6f97
James Lee <jamesl33info@gmail.com>
2018-11-05 22:51:22 +0000

    Added the css for the register form

--- src/components/forms/Register/Register.jsx
+++ src/components/forms/Register/Register.jsx
@@ -3,37 +3,61 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { renderField } from '../field.jsx'
 import { required } from '../validation.js'
+import './Register.css'
 
 class RegisterForm extends React.Component {
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit}>
-                <div>
-                    <label>User Name</label>
-                    <div>
-                        <Field name="userName" component={renderField} type="text" placeholder="User Name" validate={required}></Field>
+            <div className="register-form">
+                <form onSubmit={this.props.handleSubmit}>
+                    <p>Register</p>
+                    <div className="row">
+                        <div className="col-20">
+                            <label>User Name</label>
+                        </div>
+                        <div className="col-80">
+                            <Field name="userName" component={renderField} type="text" placeholder="User Name" validate={required}></Field>
+                        </div>
                     </div>
-                    <label>Email Address</label>
-                    <div>
-                        <Field name="emailAddress" component={renderField} type="text" placeholder="Email Address" validate={required}></Field>
+                    <div className="row">
+                        <div className="col-20">
+                            <label>Real Name</label>
+                        </div>
+                        <div className="col-80">
+                            <Field name="realName" component={renderField} type="text" placeholder="Real Name" validate={required}></Field>
+                        </div>
                     </div>
-                    <label>Confirm Email Address</label>
-                    <div>
-                        <Field name="confirmedEmailAddress" component={renderField} type="text" placeholder="Confirm Email Address" validate={required}></Field>
+                    <div className="row">
+                        <div className="col-20">
+                            <label>Password</label>
+                        </div>
+                        <div className="col-80">
+                            <Field name="userPassword" component={renderField} type="password" placeholder="Password" validate={required}/>
+                        </div>
                     </div>
-                    <label>Password</label>
-                    <div>
-                        <Field name="userPassword" component={renderField} type="password" placeholder="Password" validate={required}/>
+                    <div className="row">
+                        <div className="col-20">
+                            <label>Confirm Password</label>
+                        </div>
+                        <div className="col-80">
+                            <Field name="confirmedUserPassword" component={renderField} type="password" placeholder="Confirm Password" validate={required}/>
+                        </div>
                     </div>
-                    <label>Confirm Password</label>
-                    <div>
-                        <Field name="confirmedUserPassword" component={renderField} type="password" placeholder="Confirm Password" validate={required}/>
+                    <div className="row">
+                        <div className="col-20">
+                            <label>Email Address</label>
+                        </div>
+                        <div className="col-80">
+                            <Field name="emailAddress" component={renderField} type="text" placeholder="Email Address" validate={required}></Field>
+                        </div>
                     </div>
-                    <div>
-                        <button type="submit" disabled={this.props.pristine || this.props.submitting || this.props.invalid}>Register</button>
+                    <div className="row">
+                        <div className="right-justify">
+                            <button type="submit" disabled={this.props.pristine || this.props.submitting || this.props.invalid}>Register</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         )
     }
 }
