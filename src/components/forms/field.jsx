6c975e9c32e66b0119db8b9ea6b5bec528ae82d4
James Lee <jamesl33info@gmail.com>
2018-11-07 21:21:39 +0000

    Better form error messages

--- src/components/forms/field.jsx
+++ src/components/forms/field.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const renderField = ({ input, type, placeholder, meta: { touched, error, warning } }) => (
     <>
-        <input {...input} style={{borderColor: touched && error ? 'red' : ''}} type={type} placeholder={(touched && ((error) || (warning))) || placeholder}/>
+        <input {...input} style={{borderColor: touched && error ? 'red' : ''}} type={type} placeholder={placeholder}/>
+        {touched && ((error && <span>{'* ' + error}</span>) || (warning && <span>{'* ' + warning}</span>))}
     </>
 )
 
