b359916d689f7071be870b60e756a458e1b8a0f2
James Lee <jamesl33info@gmail.com>
2018-11-27 18:58:41 +0000

    Extract the RecipeList component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { RecipeList } from '../components/RecipeList'
 import { constants } from '../constants'
 import { actions } from '../actions'
 import './Home.css'
@@ -12,44 +13,16 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div className="recipes">
+            <>
                 <div className="recent-recipes">
-                    <h1>Recent Recipes</h1>
-                    <ul>
-                        {this.props.recent.map((recipe, recipeIndex) =>
-                            <li key={recipeIndex}>
-                                <div className="recipe-card">
-                                    <div className="recipe-preview left-justify">
-                                        <img src={constants.api.url + recipe.image.replace(/public/, '')} alt="Recipe preview"/>
-                                    </div>
-                                    <div className="recipe-meta right-justify">
-                                        <h1>{recipe.title}</h1>
-                                        <p>{recipe.description}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        )}
-                    </ul>
+                    <h1 className="headings">Recent</h1>
+                    <RecipeList recipeList={this.props.recent}/>
                 </div>
-                <div className="top-rated-recipes">
-                    <h1>Top Rated Recipes</h1>
-                    <ul>
-                        {this.props.top.map((recipe, recipeIndex) =>
-                            <li key={recipeIndex}>
-                                <div className="recipe-card">
-                                    <div className="recipe-preview left-justify">
-                                        <img src={constants.api.url + recipe.image.replace(/public/, '')} alt="Recipe preview"/>
-                                    </div>
-                                    <div className="recipe-meta right-justify">
-                                        <h1>{recipe.title}</h1>
-                                        <p>{recipe.description}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        )}
-                    </ul>
+                <div className="top-recipes">
+                    <h1 className="headings">Top Rated</h1>
+                    <RecipeList recipeList={this.props.top}/>
                 </div>
-            </div>
+            </>
         )
     }
 }
---
+++ src/components/RecipeList/RecipeList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { constants } from '../../constants'
+import { actions } from '../../actions'
+import './RecipeList.css'
+
+class RecipeList extends React.Component {
+    render() {
+        return (
+            <div className="recipes">
+                <ul>
+                    {this.props.recipeList.map((recipe, recipeIndex) =>
+                        <li key={recipeIndex}>
+                            <div className="recipe-card">
+                                <div className="recipe-preview left-justify">
+                                    <img src={constants.api.url + recipe.image.replace(/public/, '')} alt="Recipe preview"/>
+                                </div>
+                                <div className="recipe-meta right-justify">
+                                    <h1>{recipe.title}</h1>
+                                    <p>{recipe.description}</p>
+                                </div>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export { RecipeList }
